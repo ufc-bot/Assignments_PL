fc5028400f8c944219554274a69e1cc1fa590069
UFC_ <shrimali1212@gmail.com>
2024-08-27 14:12:21 +0530

    Create translator.c

---
+++ translator.c
@@ -0,0 +1,6 @@
+/*design n implement a translator which shall translate a given "select" statement of sql into an equivalent expr in relational algebra
+translator shall include:
+selection
+projection
+join and 
+set operation*/
